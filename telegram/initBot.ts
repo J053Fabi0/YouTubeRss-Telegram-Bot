84ee8369140ff381abecb828b576f8ab7143b68e
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-03-08 23:38:47 -0600

    Use grammy-runner

--- telegram/initBot.ts
+++ telegram/initBot.ts
@@ -4,15 +4,19 @@ import nextFunction from "./nextFunction.ts";
 import { sendIdComposer } from "./sendId.ts";
 import freshrss from "../freshrss/freshrss.ts";
 import messagesHandler from "./messagesHandler.ts";
+import { run, sequentialize } from "grammy-runner";
 
 const bot = new Bot(BOT_TOKEN);
 
 export default bot;
 
-bot.start({ onStart: () => console.log("Bot started") });
+// https://grammy.dev/plugins/runner#sequential-processing-where-necessary
+bot.use(sequentialize((ctx) => [ctx.chat?.id.toString(), ctx.from?.id.toString()].filter(Boolean) as string[]));
 
 bot.use(nextFunction);
 
 bot.use(freshrss);
 bot.use(messagesHandler);
 bot.use(sendIdComposer);
+
+run(bot);
